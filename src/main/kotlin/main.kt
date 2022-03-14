8c6f0119e360cdea864e10054e8b6c0ee838dfea
skvalk <skvalk@yandex.ru>
2022-03-14 13:01:40 +0300

    Доработал для многозначных чисел оканчивающихся на 11

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 21
-    val result: String = if (likes % 10 == 1 && likes != 11) "человеку." else "людям."
+    val likes = 34511
+    val result: String = if (likes % 10 == 1 && likes % 100 != 11 && likes != 11 ) "человеку." else "людям."
     println("Понравилось $likes $result")
 }
